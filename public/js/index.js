b04aebf4cb7d56862ae3f09cd86ea155243646be
Zak Stamps <theoneandonlyzako@gmail.com>
2021-07-31 16:10:44 -0700

    pushing changes for LAs

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,18 @@
+// service worker
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("/service-worker.js").then((reg) => {
+        console.log("Service worker registered.", reg);
+      });
+    });
+  }
+
 let transactions = [];
 let myChart;
+// const transactionForm = createTransactionForm();
+// const transactionApi = createTransactionApi();
+
+// initTransactions();
 
 fetch("/api/transaction")
   .then(response => {
